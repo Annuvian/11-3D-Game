5275db4c5683e2a2c500987a39425dde6044ca1a
rocfrank <rocfrank@iu.edu>
2019-12-02 17:00:23 -0500

    Adds UI elements

--- 11-3D Game/Assets/Scripts/PlayerController.cs
+++ 11-3D Game/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody ball;
     public Transform camera;
+    public Text powerUI;
+    public Text strokesUI;
     public float power = 0;
     public int strokes = 0;
     public int counter = 0;
@@ -29,42 +33,47 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown("1"))
         {
             power = 0.05f;
+            powerUI.text = "Power: 1";
         }
         if (Input.GetKeyDown("2"))
         {
             power = 0.10f;
+            powerUI.text = "Power: 2";
         }
         if (Input.GetKeyDown("3"))
         {
             power = 0.15f;
+            powerUI.text = "Power: 3";
         }
         if (Input.GetKeyDown("4"))
         {
             power = 0.20f;
+            powerUI.text = "Power: 4";
         }
         if (Input.GetKeyDown("5"))
         {
             power = 0.25f;
+            powerUI.text = "Power: 5";
         }
         if (Input.GetKeyDown("6"))
         {
             power = 0.30f;
+            powerUI.text = "Power: 6";
         }
         if (Input.GetKeyDown("7"))
         {
             power = 0.35f;
+            powerUI.text = "Power: 7";
         }
         if (Input.GetKeyDown("8"))
         {
             power = 0.40f;
+            powerUI.text = "Power: 8";
         }
         if (Input.GetKeyDown("9"))
         {
             power = 0.45f;
-        }
-        if (Input.GetKeyDown("down"))
-        {
-
+            powerUI.text = "Power: 9";
         }
         if (Input.GetMouseButtonDown(0))
         {
@@ -75,6 +84,7 @@ public class PlayerController : MonoBehaviour
     private void Putt()
     {
         strokes++;
+        strokesUI.text = "Strokes: " + Convert.ToString(strokes);
         ball.AddForce(transform.forward * power, ForceMode.Impulse);
     }
     private void OnCollisionEnter(Collision collision)
